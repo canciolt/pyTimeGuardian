2ff7661404d1d808eea54432e1d25c82fe4d8c29
Sergio A Perez <canciolt@gmail.com>
2019-05-06 17:05:16 -0400

    adding libraries and DB

---
+++ run.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+import fdb
+import xlwt
+import time
+import calendar
+import datetime
+import os
+import locale
+from settings import PATH, TIME_ZONE
+import pytz
+import smtplib
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+from email.mime.multipart import MIMEMultipart
+
+
+locale.setlocale(locale.LC_ALL, "es_CU")
+
+def main():
+    return True
+
+    
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+import os
+import pytz
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PATH = os.path.join(BASE_DIR, '/home/sergio/db-grt/TG.FDB')
+TIME_ZONE = pytz.timezone('America/Chicago')
